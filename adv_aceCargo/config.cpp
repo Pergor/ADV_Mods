952d89ab36769536ea7f684ba149c96712274b25
Belbo <adrian_verscharen@yahoo.de>
2016-05-22 13:04:50 +0200

    added resupply variants of ugvs

--- adv_aceCargo/config.cpp
+++ adv_aceCargo/config.cpp
@@ -1,13 +1,31 @@
-﻿class CfgPatches
+﻿#define macro_10 ace_cargo_space = 10;\
+		ace_cargo_hasCargo = 1;
+#define macro_20 ace_cargo_space = 20;\
+		ace_cargo_hasCargo = 1;
+#define macro_40 ace_cargo_space = 40;\
+		ace_cargo_hasCargo = 1;
+#define macro_ugv ace_refuel_fuelCapacity = 2000;\
+		supplyRadius = 10;\
+		transportAmmo = 300000;\
+		transportFuel = 2000;\
+		ace_repair_canRepair = 1;\
+		ace_cargo_space = 8;\
+		ace_cargo_hasCargo = 1;
+
+class CfgPatches
 {
     class adv_aceCargo
     {
-        units[] = {};
+        units[] = {
+			"B_UGV_resupply_F",
+			"O_UGV_resupply_F",
+			"I_UGV_resupply_F"
+		};
         weapons[] = {};
-        requiredVersion = 0.60;
-        requiredAddons[] = { "ace_cargo", "A3_Air_F_Heli_Heli_Transport_03", "A3_Armor_F_Panther", "A3_Soft_F_Gamma_HEMTT", "A3_Soft_F_EPC_Truck_03" };
-		version = "1.1";
-		versionStr = "1.1";
+        requiredVersion = 1.58;
+        requiredAddons[] = { "ace_cargo", "ace_repair", "A3_Air_F_Heli_Heli_Transport_03", "A3_Armor_F_Panther", "A3_Soft_F_Gamma_HEMTT", "A3_Soft_F_EPC_Truck_03" };
+		version = "1.02";
+		versionStr = "1.02";
 		author[] = {"[SeL] Belbo // Adrian"};
 		authorUrl = "http://spezialeinheit-luchs.de/";
     };
@@ -28,36 +46,48 @@ class CfgVehicles {
 	class B_Truck_01_mover_F;
 	class B_Truck_01_transport_F;
 	class Truck_03_base_F;
+	class B_UGV_01_F;
+	class O_UGV_01_F;
+	class I_UGV_01_F;
 	
 	class B_APC_Tracked_01_base_F: APC_Tracked_01_base_F {
-		ace_cargo_space = 10;
-		ace_cargo_hasCargo = 1;
+		macro_10
 	};
 	
 	class B_Heli_Transport_03_F: Heli_Transport_03_base_F {
-		ace_cargo_space = 40;
-		ace_cargo_hasCargo = 1;
+		macro_40
 	};	
 	class B_Heli_Transport_03_unarmed_F: Heli_Transport_03_unarmed_base_F {
-		ace_cargo_space = 40;
-		ace_cargo_hasCargo = 1;
+		macro_40
 	};	
 
 	class B_Truck_01_ammo_F: B_Truck_01_mover_F {
-		ace_cargo_space = 20;
-		ace_cargo_hasCargo = 1;
+		macro_20
 	};
     class B_Truck_01_medical_F: B_Truck_01_transport_F {
-		ace_cargo_space = 20;
-		ace_cargo_hasCargo = 1;
+		macro_20
     };
 
 	class O_Truck_03_ammo_F: Truck_03_base_F {
-		ace_cargo_space = 20;
-		ace_cargo_hasCargo = 1;
+		macro_20
 	};
     class O_Truck_03_medical_F: Truck_03_base_F {
-		ace_cargo_space = 20;
-		ace_cargo_hasCargo = 1;
+		macro_20
     };
+
+	class B_UGV_resupply_F: B_UGV_01_F
+	{
+		displayName = "UGV Stomper (Resupply)";
+		macro_ugv
+	};
+	class O_UGV_resupply_F: O_UGV_01_F
+	{
+		displayName = "UGV Saif (Resupply)";
+		macro_ugv
+	};
+	class I_UGV_resupply_F: I_UGV_01_F
+	{
+		displayName = "UGV Stomper (Resupply)";
+		macro_ugv
+	};
 };
