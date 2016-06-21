957392fe9d52e59a353d85536f40ee4eff81e366
Belbo <adrian_verscharen@yahoo.de>
2016-06-22 00:44:59 +0200

    testing with ace_refuel

--- adv_aceCargo/config.cpp
+++ adv_aceCargo/config.cpp
@@ -72,7 +72,7 @@ class CfgPatches
 		};
         weapons[] = {};
         requiredVersion = 1.58;
-        requiredAddons[] = { "ace_cargo", "ace_repair", "A3_Air_F_Heli_Heli_Transport_03", "A3_Armor_F_Panther", "A3_Soft_F_Gamma_HEMTT", "A3_Soft_F_EPC_Truck_03" };
+        requiredAddons[] = { "ace_cargo", "ace_repair", "ace_refuel","A3_Air_F_Heli_Heli_Transport_03", "A3_Armor_F_Panther", "A3_Soft_F_Gamma_HEMTT", "A3_Soft_F_EPC_Truck_03" };
 		version = "1.02";
 		versionStr = "1.02";
 		author = "[SeL] Belbo // Adrian";
