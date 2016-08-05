9190aa4a0bd54b9ed1f0a015fccbc141870d2c7e
Belbo <adrian_verscharen@yahoo.de>
2016-08-05 17:39:24 +0200

    added lee enfield soldiers and medics to groups.

--- adv_enemies_hod/cfgGroups.hpp
+++ adv_enemies_hod/cfgGroups.hpp
@@ -14,7 +14,7 @@ class CfgGroups
 				name = "Infantry";
 				class adv_enemies_hod_ft_1
 				{
-					name = "Fire Team 1";
+					name = "Fire Team 1 (w/ medic)";
 					standard_values
 					class Unit0
 					{
@@ -40,7 +40,7 @@ class CfgGroups
 					class Unit3
 					{
 						side = 0;
-						vehicle = "adv_hod_soldier_k98_1_f";
+						vehicle = "adv_hod_soldier_k98_medic_f";
 						rank = "PRIVATE";
 						position[] = {10,-10,0};
 					};
@@ -80,7 +80,7 @@ class CfgGroups
 				};
 				class adv_enemies_hod_ft_3
 				{
-					name = "Fire Team 3";
+					name = "Fire Team 3 (w/ medic)";
 					standard_values
 					class Unit0
 					{
@@ -106,7 +106,7 @@ class CfgGroups
 					class Unit3
 					{
 						side = 0;
-						vehicle = "adv_hod_soldier_m38_3_f";
+						vehicle = "adv_hod_soldier_m38_medic_f";
 						rank = "PRIVATE";
 						position[] = {10,-10,0};
 					};
@@ -140,6 +140,105 @@ class CfgGroups
 					{
 						side = 0;
 						vehicle = "adv_hod_soldier_k98_1_f";
+						rank = "PRIVATE";
+						position[] = {10,-10,0};
+					};
+				};
+				class adv_enemies_hod_ft_5
+				{
+					name = "Fire Team 5 (w/ medic)";
+					standard_values
+					class Unit0
+					{
+						side = 0;
+						vehicle = "adv_hod_soldier_mk14_1_f";
+						rank = "CORPORAL";
+						position[] = {0,0,0};
+					};
+					class Unit1
+					{
+						side = 0;
+						vehicle = "adv_hod_soldier_enf_3_f";
+						rank = "PRIVATE";
+						position[] = {5,-5,0};
+					};
+					class Unit2
+					{
+						side = 0;
+						vehicle = "adv_hod_soldier_k98_1_f";
+						rank = "PRIVATE";
+						position[] = {-5,-5,0};
+					};
+					class Unit3
+					{
+						side = 0;
+						vehicle = "adv_hod_soldier_m38_medic_f";
+						rank = "PRIVATE";
+						position[] = {10,-10,0};
+					};
+				};
+				class adv_enemies_hod_ft_6
+				{
+					name = "Fire Team 6";
+					standard_values
+					class Unit0
+					{
+						side = 0;
+						vehicle = "adv_hod_soldier_k98_2_f";
+						rank = "CORPORAL";
+						position[] = {0,0,0};
+					};
+					class Unit1
+					{
+						side = 0;
+						vehicle = "adv_hod_soldier_mk14_3_f";
+						rank = "PRIVATE";
+						position[] = {5,-5,0};
+					};
+					class Unit2
+					{
+						side = 0;
+						vehicle = "adv_hod_soldier_enf_1_f";
+						rank = "PRIVATE";
+						position[] = {-5,-5,0};
+					};
+					class Unit3
+					{
+						side = 0;
+						vehicle = "adv_hod_soldier_m38_2_f";
+						rank = "PRIVATE";
+						position[] = {10,-10,0};
+					};
+				};
+				class adv_enemies_hod_ft_7
+				{
+					name = "Fire Team 7 (w/ medic)";
+					standard_values
+					class Unit0
+					{
+						side = 0;
+						vehicle = "adv_hod_soldier_k98_3_f";
+						rank = "CORPORAL";
+						position[] = {0,0,0};
+					};
+					class Unit1
+					{
+						side = 0;
+						vehicle = "adv_hod_soldier_mk14_2_f";
+						rank = "PRIVATE";
+						position[] = {5,-5,0};
+					};
+					class Unit2
+					{
+						side = 0;
+						vehicle = "adv_hod_soldier_enf_1_f";
+						rank = "PRIVATE";
+						position[] = {-5,-5,0};
+					};
+					class Unit3
+					{
+						side = 0;
+						vehicle = "adv_hod_soldier_m38_medic_f";
 						rank = "PRIVATE";
 						position[] = {10,-10,0};
 					};
