f6b3ed3bae00019478a514f9a283940b91411018
Belbo <adrian_verscharen@yahoo.de>
2017-03-14 17:58:20 +0100

    fixed wrong mags, added flare cfg for 82mm mortar.

--- adv_configsVanilla/config.cpp
+++ adv_configsVanilla/config.cpp
@@ -6,18 +6,10 @@ class CfgPatches
         weapons[] = {};
         requiredVersion = 1.60;
         requiredAddons[] = {
-			"A3_Weapons_F",
-			"A3_Weapons_F_LongRangeRifles_M320",
-			"A3_Weapons_F_LongRangeRifles_GM6",
-			"A3_Weapons_F_EPB_LongRangeRifles_M320",
-			"A3_Weapons_F_EPA_LongRangeRifles_GM6",
-			"A3_Weapons_F_Bootcamp_LongRangeRifles_M320",
-			"A3_Weapons_F_Bootcamp_LongRangeRifles_GM6",
-			"A3_Weapons_F_Mark_LongRangeRifles_M320",
-			"A3_Weapons_F_Mark_LongRangeRifles_GM6_camo"
+			"A3_Weapons_F"
 		};
-		version = "1.01";
-		versionStr = "1.01";
+		version = "1.02";
+		versionStr = "1.02";
 		author = "[SeL] Belbo // Adrian";
 		authorUrl = "http://spezialeinheit-luchs.de/";
     };
@@ -25,6 +17,7 @@ class CfgPatches
 
 class CfgWeapons
 {
+	/*
 	class Rifle;
 	class Rifle_Base_F: Rifle {
 		class WeaponSlotsInfo;
@@ -32,10 +25,12 @@ class CfgWeapons
 	class Rifle_Long_Base_F: Rifle_Base_F {
 		class WeaponSlotsInfo;
 	};
-	class LMG_03_base_F: Rifle_Long_Base_F {
+	*/
+	class LMG_03_base_F;
+	class LMG_03_F: LMG_03_base_F {
 		magazines[] += {
-			"200Rnd_556x45_Box_F","200Rnd_556x45_Box_Red_F","200Rnd_556x45_Box_Tracer_F","200Rnd_556x45_Box_Tracer_Red_F"
-			,"30Rnd_556x45_Stanag","30Rnd_556x45_Stanag_green","30Rnd_556x45_Stanag_red","30Rnd_556x45_Stanag_Tracer_Red","30Rnd_556x45_Stanag_Tracer_Green","30Rnd_556x45_Stanag_Tracer_Yellow"
+			"200Rnd_556x45_Box_F","200Rnd_556x45_Box_Red_F","200Rnd_556x45_Box_Tracer_F","200Rnd_556x45_Box_Tracer_Red_F",
+			"30Rnd_556x45_Stanag","30Rnd_556x45_Stanag_green","30Rnd_556x45_Stanag_red","30Rnd_556x45_Stanag_Tracer_Red","30Rnd_556x45_Stanag_Tracer_Green","30Rnd_556x45_Stanag_Tracer_Yellow"
 		};
 	};
 	
@@ -58,4 +53,19 @@ class CfgWeapons
 		};
 	};
 	*/
+};
+
+class CfgAmmo {
+	class FlareCore;
+	class Flare_82mm_AMOS_White : FlareCore {
+        intensity = 120000;
+        flareSize = 18;
+		timeToLive = 60;
+		aimAboveTarget[] = {20,40,80,120,160,200,240};
+		aimAboveDefault = 4;
+	};
+	class SmokeShell;
+	class SmokeShellArty : SmokeShell {
+		effectsSmoke = "SmokeShellWhiteEffect";
+	};
 };
