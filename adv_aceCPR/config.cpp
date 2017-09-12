147f3b7c8a174dc752a3b9f9ea09ddbcd6f3a2a8
Belbo <adrian_verscharen@yahoo.de>
2017-09-12 14:10:26 +0200

    MATCHED: version number to 1.4.4

--- adv_aceCPR/config.cpp
+++ adv_aceCPR/config.cpp
@@ -12,8 +12,8 @@
         requiredAddons[] = {
 			"ace_medical"
 		};
-		version = "1.4.3";
-		versionStr = "1.4.3";
+		version = "1.4.4";
+		versionStr = "1.4.4";
 		author = "[SeL] Belbo // Adrian";
 		authorUrl = "http://spezialeinheit-luchs.de/";
     };
