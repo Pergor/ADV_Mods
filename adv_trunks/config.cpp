6094dfc8f274f7dce3e825294b6c68e470c36cb3
Belbo <adrian_verscharen@yahoo.de>
2018-05-08 23:07:36 +0200

    ADDED: adv_trunks - making the suitcase and other cases usable.

---
+++ adv_trunks/config.cpp
@@ -0,0 +1,121 @@
+class CfgPatches
+{
+    class adv_trunks
+    {
+        units[] = {
+			"adv_trunk_suitcase_f"
+			,"adv_trunk_metalcase_01_f"
+			,"adv_trunk_metalcase_02_f"
+			,"adv_trunk_plasticcase_01_f"
+			,"adv_trunk_plasticcase_01_grey_f"
+			,"adv_trunk_plasticcase_02_f"
+			,"adv_trunk_plasticcase_02_grey_f"
+		};
+        weapons[] = {};
+        requiredVersion = 1.82;
+        requiredAddons[] = {
+			"A3_Structures_F_Items_Luggage"
+			,"A3_Weapons_F_Ammoboxes"
+		};
+		version = "1.0.0";
+		versionStr = "1.0.0";
+		author = "[SeL] Belbo // Adrian";
+		authorUrl = "http://spezialeinheit-luchs.de/";
+    };
+};
+
+#define MACRO_ADDWEAPON(WEAPON,COUNT) class _xx_##WEAPON { \
+    weapon = #WEAPON; \
+    count = COUNT; \
+}
+
+#define MACRO_ADDMAGAZINE(MAGAZINE,COUNT) class _xx_##MAGAZINE { \
+    magazine = #MAGAZINE; \
+    count = COUNT; \
+}
+
+#define MACRO_ADDITEM(ITEM,COUNT) class _xx_##ITEM { \
+    name = #ITEM; \
+    count = COUNT; \
+}
+
+#define MACRO_ADDBACKPACK(BACKPACK,COUNT) class _xx_##BACKPACK { \
+    backpack = #BACKPACK; \
+    count = COUNT; \
+}
+
+#define MACRO_EDITORIAL author = "Spezialeinheit Luchs";
+
+class CfgVehicles {
+	class NATO_Box_Base;
+	class adv_trunk_suitcase_f: NATO_Box_Base {
+		MACRO_EDITORIAL
+		displayName = "[ADV] Suitcase";
+		model = "\A3\Structures_F\Items\Luggage\Suitcase_F.p3d";
+		editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\Land_Suitcase_F.jpg";
+		hiddenSelections[] = {};
+		hiddenSelectionsTextures[] = {};
+		destrType = "DestructNo";
+		editorCategory = "EdCat_Supplies";
+		vehicleClass = "Ammo";
+		scope = 2;
+		curatorScope = 2;
+		mapSize = 1.81;
+		maximumLoad = 600;
+		transportMaxWeapons = 2;
+		transportMaxMagazines = 12;
+		transportMaxBackpacks = 2;
+		class TransportWeapons {};
+		class TransportMagazines {};
+		class TransportItems {};
+		class TransportBackpacks {};
+	};
+	
+	class adv_trunk_metalcase_01_f: adv_trunk_suitcase_f {
+		displayName = "[ADV] Metal Case (Small)";
+		model = "\A3\Structures_F_Heli\Items\Luggage\MetalCase_01_small_F.p3d";
+		editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\Land_MetalCase_01_small_F.jpg";
+	};
+	class adv_trunk_metalcase_02_f: adv_trunk_suitcase_f {
+		displayName = "[ADV] Metal Case (Medium)";
+		model = "\A3\Structures_F_Heli\Items\Luggage\MetalCase_01_medium_F.p3d";
+		editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\Land_MetalCase_01_medium_F.jpg";
+		maximumLoad = 1000;
+		transportMaxWeapons = 6;
+		transportMaxMagazines = 32;
+		transportMaxBackpacks = 6;
+	};
+	
+	class adv_trunk_plasticcase_01_f: adv_trunk_suitcase_f {
+		displayName = "[ADV] Plastic Case (Small)";
+		model = "\A3\Structures_F_Heli\Items\Luggage\PlasticCase_01_small_F.p3d";
+		editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\Land_PlasticCase_01_small_F.jpg";
+		hiddenselections[] = {"camo"};
+		hiddenselectionsTextures[] = {"a3\structures_f_heli\items\luggage\data\plasticcase_01_co.paa"};
+		maximumLoad = 300;
+		transportMaxWeapons = 1;
+		transportMaxMagazines = 6;
+		transportMaxBackpacks = 1;
+	};
+	class adv_trunk_plasticcase_01_grey_f: adv_trunk_plasticcase_01_f {
+		displayName = "[ADV] Plastic Case (Small, Grey)";
+		hiddenSelectionsTextures[] = {"a3\Props_F_Orange\Humanitarian\Supplies\Data\PlasticCase_01_gray_CO.paa"};
+		editorPreview = "\A3\EditorPreviews_F_Orange\Data\CfgVehicles\Land_PlasticCase_01_small_gray_F.jpg";
+	};
+	
+	class adv_trunk_plasticcase_02_f: adv_trunk_plasticcase_01_f {
+		displayName = "[ADV] Plastic Case (Medium)";
+		model = "\A3\Structures_F_Heli\Items\Luggage\PlasticCase_01_medium_F.p3d";
+		hiddenselectionsTextures[] = {"a3\structures_f_heli\items\luggage\data\plasticcase_01_co.paa"};
+		editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\Land_PlasticCase_01_medium_F.jpg";
+		maximumLoad = 600;
+		transportMaxWeapons = 2;
+		transportMaxMagazines = 12;
+		transportMaxBackpacks = 2;
+	};
+	class adv_trunk_plasticcase_02_grey_f: adv_trunk_plasticcase_02_f {
+		displayName = "[ADV] Plastic Case (Medium, Grey)";
+		hiddenSelectionsTextures[] = {"a3\Props_F_Orange\Humanitarian\Supplies\Data\PlasticCase_01_gray_CO.paa"};
+		editorPreview = "\A3\EditorPreviews_F_Orange\Data\CfgVehicles\Land_PlasticCase_01_medium_gray_F.jpg";
+	};
+};
