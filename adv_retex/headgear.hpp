51ebb189ca24f24241b3be1951dce8e97e52f8d3
Belbo <adrian_verscharen@yahoo.de>
2018-05-24 15:06:49 +0200

    ADDED: wdl camo helmet.

---
+++ adv_retex/headgear.hpp
@@ -0,0 +1,24 @@
+﻿class H_PASGT_neckprot_blue_press_F;
+
+class H_PASGT_neckprot_black_F: H_PASGT_neckprot_blue_press_F {
+	hiddenSelectionsTextures[] = {"\A3\Characters_F_Orange\Headgear\Data\H_PASGT_black_CO.paa"};
+	scope = 2;
+	displayName = "Basic Helmet (Black - Neck Protection)";
+};
+class H_PASGT_neckprot_olive_F: H_PASGT_neckprot_blue_press_F {
+	hiddenSelectionsTextures[] = {"\A3\Characters_F_Orange\Headgear\Data\H_PASGT_olive_CO.paa"};
+	scope = 2;
+	displayName = "Basic Helmet (Olive - Neck Protection)";
+};
+class H_PASGT_neckprot_blue_F: H_PASGT_neckprot_blue_press_F {
+	hiddenSelectionsTextures[] = {"\A3\Characters_F_Orange\Headgear\Data\H_PASGT_blue_CO.paa"};
+	scope = 2;
+	displayName = "Basic Helmet (Blue - Neck Protection)";
+};
+
+class H_HelmetIA;
+class H_HelmetIA_wdl: H_HelmetIA {
+	hiddenSelectionsTextures[] = {"adv_retex\textures\equipment\headgear_helmet_canvas_wdl_co.paa"};
+	scope = 2;
+	displayName = "Modular Helmet (Woodland)";
+};
