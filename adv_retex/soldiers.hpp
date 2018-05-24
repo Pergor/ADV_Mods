16c8cd31833dd5a10d2c84cf304485207b47acfc
Belbo <adrian_verscharen@yahoo.de>
2018-05-24 15:06:17 +0200

    IMPROVED: added hidden textures to uniforms.

--- adv_retex/soldiers.hpp
+++ adv_retex/soldiers.hpp
@@ -3,3 +3,32 @@
 class I_medic_F: I_Soldier_02_F {
 	backpack = "B_AssaultPack_dgtl_Medic";
 };
+
+#define macro_uniformDummy(UCL,HST,MDL) scope = 1;\
+	uniformClass = #UCL;\
+	hiddenSelections[] = {"Camo"};\
+	hiddenSelectionsTextures[] = {HST};\
+	model = MDL;
+
+class B_soldier_f;
+class adv_retex_dummy_mcam_worn: B_soldier_F {
+	macro_uniformDummy(U_B_CombatUniform_mcam_worn,"a3\characters_f_epb\BLUFOR\Data\clothing1_dirty_co.paa","\A3\characters_F\BLUFOR\b_soldier_01.p3d")
+};
+class adv_retex_dummy_wdl: B_soldier_F {
+	macro_uniformDummy(adv_retex_u_CombatUniform_wdl,"a3\characters_f\BLUFOR\Data\clothing_wdl_co.paa","\A3\characters_F\BLUFOR\b_soldier_01.p3d")
+};
+class adv_retex_dummy_wdl_vest: B_soldier_F {
+	macro_uniformDummy(adv_retex_u_CombatUniform_wdl_vest,"a3\characters_f\BLUFOR\Data\clothing_wdl_co.paa","\A3\characters_F\BLUFOR\b_soldier_03.p3d")
+};
+class adv_retex_dummy_wdl_tshirt: B_soldier_F {
+	macro_uniformDummy(adv_retex_u_CombatUniform_wdl_tshirt,"a3\characters_f\BLUFOR\Data\clothing_wdl_co.paa","\A3\characters_F\BLUFOR\b_soldier_02.p3d")
+};
+class adv_retex_dummy_sgg: B_soldier_F {
+	macro_uniformDummy(adv_retex_u_CombatUniform_sgg,"a3\characters_f\BLUFOR\Data\clothing_sage_co.paa","\A3\characters_F\BLUFOR\b_soldier_01.p3d")
+};
+class adv_retex_dummy_sgg_vest: B_soldier_F {
+	macro_uniformDummy(adv_retex_u_CombatUniform_sgg_vest,"a3\characters_f\BLUFOR\Data\clothing_sage_co.paa","\A3\characters_F\BLUFOR\b_soldier_03.p3d")
+};
+class adv_retex_dummy_sgg_tshirt: B_soldier_F {
+	macro_uniformDummy(adv_retex_u_CombatUniform_sgg_tshirt,"a3\characters_f\BLUFOR\Data\clothing_sage_co.paa","\A3\characters_F\BLUFOR\b_soldier_02.p3d")
+};
---
+++ adv_retex/uniforms.hpp
@@ -0,0 +1,42 @@
+﻿class Uniform_Base;
+class U_B_CombatUniform_mcam;
+class U_B_CombatUniform_mcam_vest;
+class U_B_CombatUniform_mcam_tshirt;
+class UniformItem;
+
+#define macro_uniformInfo(TEXTURE,CLASS) hiddenSelectionsTextures[] = {TEXTURE};\
+	class ItemInfo: UniformItem {\
+		uniformModel = "-";\
+		uniformClass = #CLASS;\
+		containerClass = "Supply40";\
+		mass = 40;\
+	};
+
+class U_B_CombatUniform_mcam_worn: Uniform_Base {
+	macro_uniformInfo("a3\characters_f_epb\BLUFOR\Data\clothing1_dirty_co.paa",adv_retex_dummy_mcam_worn)
+};
+class adv_retex_u_CombatUniform_wdl: U_B_CombatUniform_mcam {
+	displayName = "Combat Fatigues (Woodland)";
+	macro_uniformInfo("a3\characters_f\BLUFOR\Data\clothing_wdl_co.paa",adv_retex_dummy_wdl)
+};
+class adv_retex_u_CombatUniform_wdl_vest: U_B_CombatUniform_mcam_vest {
+	displayName = "Recon Fatigues (Woodland)";
+	macro_uniformInfo("a3\characters_f\BLUFOR\Data\clothing_wdl_co.paa",adv_retex_dummy_wdl_vest)
+};
+class adv_retex_u_CombatUniform_wdl_tshirt: U_B_CombatUniform_mcam_tshirt {
+	displayName = "Combat Fatigues (Woodland) (Tee)";
+	macro_uniformInfo("a3\characters_f\BLUFOR\Data\clothing_wdl_co.paa",adv_retex_dummy_wdl_tshirt)
+};
+
+class adv_retex_u_CombatUniform_sgg: U_B_CombatUniform_mcam {
+	displayName = "Combat Fatigues (Sage)";
+	macro_uniformInfo("a3\characters_f\BLUFOR\Data\clothing_sage_co.paa",adv_retex_dummy_sgg)
+};
+class adv_retex_u_CombatUniform_sgg_vest: U_B_CombatUniform_mcam {
+	displayName = "Recon Fatigues (Sage)";
+	macro_uniformInfo("a3\characters_f\BLUFOR\Data\clothing_sage_co.paa",adv_retex_dummy_sgg_vest)
+};
+class adv_retex_u_CombatUniform_sgg_tshirt: U_B_CombatUniform_mcam {
+	displayName = "Combat Fatigues (Sage) (Tee)";
+	macro_uniformInfo("a3\characters_f\BLUFOR\Data\clothing_sage_co.paa",adv_retex_dummy_sgg_tshirt)
+};
