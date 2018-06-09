54ff67f525364709a9ab1db10dadde5a69ffeef9
Belbo <adrian_verscharen@yahoo.de>
2018-06-09 03:11:01 +0200

    ADDED: vests with sel logos.

---
+++ sel_vests/config.cpp
@@ -0,0 +1,25 @@
+﻿class CfgPatches
+{
+    class sel_vests
+    {
+        units[] = {};
+        weapons[] = {
+			"adv_PlateCarrier1_nobelt_rgr"
+			,"adv_PlateCarrier1_sel"
+			,"adv_PlateCarrier2_sel"
+			,"adv_PlateCarrier1_nobelt_sel"
+		};
+        requiredVersion = 1.82;
+        requiredAddons[] = {
+			"A3_Characters_F"
+		};
+		version = "1.0.0";
+		versionStr = "1.0.0";
+		author = "[SeL] Belbo // Adrian";
+		authorUrl = "http://spezialeinheit-luchs.de/";
+    };
+};
+
+class cfgWeapons {
+	#include "vests.hpp"
+};
---
+++ sel_vests/vests.hpp
@@ -0,0 +1,90 @@
+﻿#define macro_vest_standard(NAME) displayName = NAME;\
+	author = "Spezialeinheit Luchs";\
+	scope = 2;
+
+#define macro_vest_texture(TEXTURE) hiddenSelections[] = {"Camo"};\
+	hiddenSelectionsTextures[] = {TEXTURE};
+
+class Vest_NoCamo_Base;
+class V_PlateCarrier1_rgr: Vest_NoCamo_Base {
+	class ItemInfo;
+};
+class V_PlateCarrier2_rgr: V_PlateCarrier1_rgr {
+	class ItemInfo;
+};
+
+
+class adv_PlateCarrier1_nobelt_rgr: V_PlateCarrier1_rgr {
+	macro_vest_standard("Carrier Lite (No belt)")
+	macro_vest_texture("sel_vests\textures\vests_co_rgr.paa")
+	model = "\A3\Characters_F_EPA\BLUFOR\equip_b_vest_kerry.p3d";
+	class ItemInfo: ItemInfo {
+		macro_vest_texture("sel_vests\textures\vests_co_rgr.paa")
+		uniformModel = "\A3\Characters_F_EPA\BLUFOR\equip_b_vest_kerry.p3d";
+	};
+};
+class adv_PlateCarrier1_nobelt_wdl: adv_PlateCarrier1_nobelt_rgr {
+	macro_vest_standard("Carrier Lite (No belt) Woodland")
+	macro_vest_texture("sel_vests\textures\vests_co_wdl.paa")
+	class ItemInfo: ItemInfo {
+		macro_vest_texture("sel_vests\textures\vests_co_wdl.paa")
+	};	
+};
+class adv_PlateCarrier1_nobelt_sel: adv_PlateCarrier1_nobelt_rgr {
+	macro_vest_standard("Carrier Lite (No belt) (SeL)")
+	macro_vest_texture("sel_vests\textures\vests_co_sel.paa")
+	class ItemInfo: ItemInfo {
+		macro_vest_texture("sel_vests\textures\vests_co_sel.paa")
+	};	
+};
+class adv_PlateCarrier1_nobelt_sel_wdl: adv_PlateCarrier1_nobelt_rgr {
+	macro_vest_standard("Carrier Lite (No belt) (Woodland - SeL)")
+	macro_vest_texture("sel_vests\textures\vests_co_sel_wdl.paa")
+	class ItemInfo: ItemInfo {
+		macro_vest_texture("sel_vests\textures\vests_co_sel_wdl.paa")
+	};	
+};
+
+class adv_PlateCarrier1_sel: V_PlateCarrier1_rgr {
+	macro_vest_standard("Carrier Lite (SeL)")
+	macro_vest_texture("sel_vests\textures\vests_co_sel.paa")
+	class ItemInfo: ItemInfo {
+		macro_vest_texture("sel_vests\textures\vests_co_sel.paa")
+	};	
+};
+class adv_PlateCarrier1_wdl: V_PlateCarrier1_rgr {
+	macro_vest_standard("Carrier Lite (Woodland)")
+	macro_vest_texture("sel_vests\textures\vests_co_wdl.paa")
+	class ItemInfo: ItemInfo {
+		macro_vest_texture("sel_vests\textures\vests_co_wdl.paa")
+	};	
+};
+class adv_PlateCarrier1_sel_wdl: V_PlateCarrier1_rgr {
+	macro_vest_standard("Carrier Lite (Woodland - SeL)")
+	macro_vest_texture("sel_vests\textures\vests_co_sel_wdl.paa")
+	class ItemInfo: ItemInfo {
+		macro_vest_texture("sel_vests\textures\vests_co_sel_wdl.paa")
+	};	
+};
+
+class adv_PlateCarrier2_sel: V_PlateCarrier2_rgr {
+	macro_vest_standard("Carrier Rig (SeL)")
+	macro_vest_texture("sel_vests\textures\vests_co_sel.paa")
+	class ItemInfo: ItemInfo {
+		macro_vest_texture("sel_vests\textures\vests_co_sel.paa")
+	};	
+};
+class adv_PlateCarrier2_wdl: V_PlateCarrier2_rgr {
+	macro_vest_standard("Carrier Rig (Woodland)")
+	macro_vest_texture("sel_vests\textures\vests_co_wdl.paa")
+	class ItemInfo: ItemInfo {
+		macro_vest_texture("sel_vests\textures\vests_co_wdl.paa")
+	};	
+};
+class adv_PlateCarrier2_sel_wdl: V_PlateCarrier2_rgr {
+	macro_vest_standard("Carrier Rig (Woodland - SeL)")
+	macro_vest_texture("sel_vests\textures\vests_co_sel_wdl.paa")
+	class ItemInfo: ItemInfo {
+		macro_vest_texture("sel_vests\textures\vests_co_sel_wdl.paa")
+	};	
+};
