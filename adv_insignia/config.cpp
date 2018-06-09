7e75ea15eeb8d79f408af2ef02601bf5b7d7a171
Belbo <adrian_verscharen@yahoo.de>
2018-06-09 03:09:55 +0200

    IMPROVED: added new special role insignia.

--- adv_insignia/config.cpp
+++ adv_insignia/config.cpp
@@ -6,8 +6,8 @@
         weapons[] = {};
         requiredVersion = 0.60;
         requiredAddons[] = {};
-		version = "1.1";
-		versionStr = "1.1";
+		version = "1.1.1";
+		versionStr = "1.1.1";
 		author = "[SeL] Belbo // Adrian";
 		authorUrl = "http://spezialeinheit-luchs.de/";
     };
@@ -23,22 +23,45 @@ class CfgFunctions {
 	};
 };
 
+#define macro_insignia(NAME,TEXTURE) displayName = NAME;\
+		texture = TEXTURE;\
+		author = "[SeL] Belbo";\
+		textureVehicle = "";
+
 class CfgUnitInsignia
 {
 	//general insignia
 	class ADV_insignia_medic
 	{
-		displayName = "Insignia Medic (fabric)";
-		author = "[SeL] Belbo // Adrian";
-		texture = "\adv_insignia\img\ADV_medic.paa";
-		textureVehicle = "";
+		macro_insignia("Insignia Medic (fabric)","\adv_insignia\img\ADV_medic.paa")
 	};
 	class ADV_insignia_medic_veh
 	{
-		displayName = "Insignia Medic Vehicle";
-		author = "[SeL] Belbo // Adrian";
-		texture = "\adv_insignia\img\ADV_medic_veh.paa";
-		textureVehicle = "";
+		macro_insignia("Insignia Medic Vehicle","\adv_insignia\img\ADV_medic_veh.paa")
+	};
+	class ADV_insignia_eod
+	{
+		macro_insignia("Insignia EOD","\adv_insignia\img\adv_eod.paa")
+	};
+	class ADV_insignia_fac
+	{
+		macro_insignia("Insignia FAC","\adv_insignia\img\adv_fac.paa")
+	};
+	class ADV_insignia_jfo
+	{
+		macro_insignia("Insignia JFO","\adv_insignia\img\adv_jfo.paa")
+	};
+	class ADV_insignia_jtac
+	{
+		macro_insignia("Insignia JTAC","\adv_insignia\img\adv_jtac.paa")
+	};
+	class ADV_insignia_ucp_jtac
+	{
+		macro_insignia("Insignia JTAC (UCP)","\adv_insignia\img\adv_ucp_jtac.paa")
+	};
+	class ADV_insignia_ucp_eod
+	{
+		macro_insignia("Insignia EOD (UCP)","\adv_insignia\img\adv_ucp_eod.paa")
 	};
 
 	//usarmy insignia
