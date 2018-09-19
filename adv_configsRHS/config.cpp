7869941ad332974051bf8eac8551a5f635144c8a
Belbo <adrian_verscharen@yahoo.de>
2018-09-19 22:36:47 +0200

    ADDED: configsRHS to make vehicles not noise cancelled.

---
+++ adv_configsRHS/config.cpp
@@ -0,0 +1,29 @@
+class CfgPatches
+{
+    class adv_configsRHS
+    {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.82;
+        requiredAddons[] = {
+			"rhsusf_c_Caiman"
+			,"rhsusf_c_Cougar"
+			,"rhsusf_c_HEMTT_A2"
+			,"rhsusf_c_HEMTT_A2"
+			,"rhsusf_c_himars"
+			,"rhsusf_c_hmmwv"
+			,"rhsusf_c_mrzr"
+			,"rhsusf_c_mtvr"
+			,"rhsusf_c_rg33"
+			,"rhsusf_c_RG33L"
+		};
+		version = "1.0.0";
+		versionStr = "1.0.0";
+		author = "[SeL] Belbo // Adrian";
+		authorUrl = "http://spezialeinheit-luchs.de/";
+    };
+};
+
+class CfgVehicles {
+	#include "vehicles.hpp"
+};
