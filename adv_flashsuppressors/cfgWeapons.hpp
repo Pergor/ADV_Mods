33c328d55341f2ab67b5f419cde60400d1df68f1
Belbo <adrian_verscharen@yahoo.de>
2018-09-21 13:03:59 +0200

    ADDED: adv_flashsuppressors

---
+++ adv_flashsuppressors/cfgWeapons.hpp
@@ -0,0 +1,12 @@
+class asdg_MuzzleSlot;
+
+class asdg_MuzzleSlot_58: asdg_MuzzleSlot {
+	class compatibleItems {
+		ACE_muzzle_mzls_L = 1;
+	};
+};
+
+/*
+class cfgWeapons {
+};
+*/
---
+++ adv_flashsuppressors/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches
+{
+    class adv_flashsuppressors
+    {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.82;
+        requiredAddons[] = {"ace_flashsuppressors"};
+		version = "1.0.0";
+		versionStr = "1.0.0";
+		author = "[SeL] Belbo // Adrian";
+		authorUrl = "http://spezialeinheit-luchs.de/";
+    };
+};
+
+#include "cfgWeapons.hpp"
